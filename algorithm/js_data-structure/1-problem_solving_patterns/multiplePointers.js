e2ecfbc42d62a7c4ccd2eea944a4011ee536fa75
Fauzan Valdera <f.valdera@yahoo.co.id>
2021-02-10 04:49:38 +0700

    ha

---
+++ algorithm/js_data-structure/1-problem_solving_patterns/multiplePointers.js
@@ -0,0 +1,23 @@
+// Creating pointers or values that correspond to an index or position
+// and move forwards the beginning, end, or middle based on certain condition.
+
+// Very efficient for solving problems with minimal space complexity as well
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left] + arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return undefined;
+}
+
+sumZero([-3, -2, -1, 0, 1, 2, 3]); // [-3,3]
+sumZero([-2, 0, 1, 3]); // undefined
